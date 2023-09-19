52d2cc4714c4ff02a432f34b79c2e39d1321271f
DawnSky <98626869+Dawn5ky@users.noreply.github.com>
2023-09-19 18:14:18 +0800

    Update and rename entrypoint.sh to loader.sh

---
+++ loader.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+WEB_USERNAME=${WEB_USERNAME:-'admin'}
+WEB_PASSWORD=${WEB_PASSWORD:-'password'}
+
+generate_nezha() {
+  cat > nezha.sh << EOF
+#!/usr/bin/env bash
+
+# 哪吒的三个参数
+NEZHA_SERVER=${NEZHA_SERVER}
+NEZHA_PORT=${NEZHA_PORT}
+NEZHA_KEY=${NEZHA_KEY}
+TLS=${NEZHA_TLS:+'--tls'}
+
+# 检测是否已运行
+check_run() {
+  [[ \$(pgrep -lafx nezha-agent) ]] && echo "哪吒客户端正在运行中" && exit
+}
+
+# 三个变量不全则不安装哪吒客户端
+check_variable() {
+  [[ -z "\${NEZHA_SERVER}" || -z "\${NEZHA_PORT}" || -z "\${NEZHA_KEY}" ]] && exit
+}
+
+# 下载最新版本 Nezha Agent
+download_agent() {
+  if [ ! -e nezha-agent ]; then
+    URL=\$(wget -qO- "https://api.github.com/repos/nezhahq/agent/releases/latest" | grep -o "https.*linux_amd64.zip")
+    URL=\${URL:-https://github.com/nezhahq/agent/releases/download/v0.15.6/nezha-agent_linux_amd64.zip}
+    wget \${URL}
+    unzip -qod ./ nezha-agent_linux_amd64.zip && rm -f nezha-agent_linux_amd64.zip
+  fi
+}
+
+# 运行 Nezha 客户端
+run() {
+  [ -e nezha-agent ] && nohup ./nezha-agent -s \${NEZHA_SERVER}:\${NEZHA_PORT} -p \${NEZHA_KEY} \${TLS} >/dev/null 2>&1 &
+}
+
+check_run
+check_variable
+download_agent
+run
+EOF
+}
+
+# 由于 Glitch 用户空间只有 200MB，故每5秒自动删除垃圾文件
+generate_autodel() {
+  cat > auto_del.sh <<EOF
+while true; do
+  rm -rf /app/.git
+  sleep 5
+done
+EOF
+}
+
+generate_nezha
+generate_autodel
+
+[ -e nezha.sh ] && bash nezha.sh
+[ -e auto_del.sh ] && bash auto_del.sh
